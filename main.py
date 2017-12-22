ff6906ad32fe7471b86b1cc65001aae348425c2a
cristian <cdomingupt@alumnos.unex.es>
2017-12-22 12:21:06 +0100

    Finished

--- main.py
+++ main.py
@@ -18,3 +18,16 @@ model = Sequential()
 model.add(Dense(12, input_dim=8,activation="relu")) #12 neurons
 model.add(Dense(8, activation="relu")) #8 neurons
 model.add(Dense(1,activation="sigmoid"))#1 neuron
+
+#Compile model
+#loss= function to evaluate a set of weights
+#optimizer= adam, optimization algorithm
+#metrics= metrics used to train the model
+model.compile(loss="binary_crossentropy",optimizer="adam",metrics=["accuracy"])
+
+# Fit the model
+model.fit(input, output, epochs=150, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(input, output)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
